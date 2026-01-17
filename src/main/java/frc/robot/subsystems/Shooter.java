d27bc9b840ed82ca65119dfa1f71d41af6a1b56a
forgotentime <michaelacovington@hotmail.com>
2026-01-17 14:08:33 -0500

    commented and velocity encoder

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,11 +1,6 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
-
+//Imports(figure it out Einstein)
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
@@ -21,37 +16,41 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 
 public class Shooter extends SubsystemBase {
+  //defininine
   private SparkMax motorShooter;
   private SparkMaxConfig motorShooterConfig;
   private SparkClosedLoopController sparkControl;
   private RelativeEncoder encoder;
   
-  /** Creates a new Shooter. */
   public Shooter() {
+    //define motor and its controller and their effects.(/°W^)
     motorShooter = new SparkMax(SHOOTER, MotorType.kBrushless);
     motorShooterConfig = new SparkMaxConfig();
     sparkControl = motorShooter.getClosedLoopController();
     encoder = motorShooter.getEncoder();
-
+//PID woot! woot!
     motorShooterConfig
       .idleMode(IdleMode.kBrake)
       .smartCurrentLimit(60)
       .closedLoop.positionWrappingEnabled(true)
       .pid(.005, 0, 0);
-
+    motorShooterConfig.encoder.velocityConversionFactor(VELOCITY_CONVERT);
+//config
     motorShooter.configure(motorShooterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 public void stop(){
+  // We need to put a stop to this
   motorShooter.stopMotor();
 }
 public void spinShoot(){
+  //largely an empty command just here to prevent an ERROR
   motorShooter.set(.4);
 }
 public void PIDShoot(double fireSpeed){
+  //here is what casues the shot
   sparkControl.setSetpoint(fireSpeed, ControlType.kVelocity);
 }
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 }
